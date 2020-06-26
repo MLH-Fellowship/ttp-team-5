67c503b26b2520840c9ed121774220a6736bf478
Summer <tiantianxia@MacBook-Pro.local>
2020-06-26 13:05:31 -0400

    search function works

--- src/components/views/PlayerSearchView.js
+++ src/components/views/PlayerSearchView.js
@@ -4,19 +4,18 @@ import PropTypes from "prop-types"
 const PlayerSearchView = (props) => {
   console.log("props.player = ", props.allPlayers)
   return(
-    // <div className="container">
-    //   {props.api.players.map(player =>
-    //     <div key={player.id}>
-    //       <h5>First Name: {player.firstname}</h5>
-    //       <h5>Last Name: {player.lastname}</h5>
-    //       <h5>Age: {player.age}</h5>
-    //       <h5>Position: {player.position}</h5>
-    //       <h5>Height: {player.height}</h5>
-    //       <h5>Weight: {player.weight}</h5>
-    //     // </div>
-    //   )}
-    // </div>
-    <></>
+    <div className="container">
+      {props.allPlayers.map(player =>
+        <div key={player.id}>
+          <h5>First Name: {player.firstname}</h5>
+          <h5>Last Name: {player.lastname}</h5>
+          <h5>Age: {player.age}</h5>
+          <h5>Position: {player.position}</h5>
+          <h5>Height: {player.height}</h5>
+          <h5>Weight: {player.weight}</h5>
+         </div>
+      )}
+    </div>
   )
 }
 
